fe0ac613d82110e80f185df981a1ebb33211442a
Anis <ayed.aniss@hotmail.com>
2021-12-06 12:09:21 +0100

    Q3B-5min

--- src/app/user/logout/logout.component.ts
+++ src/app/user/logout/logout.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UserModule} from "../user.module";
+import {UserService} from "../user-service.service";
+import {Router} from "@angular/router";
+import {Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.css']
 })
-export class LogoutComponent implements OnInit {
-
-  constructor() { }
+export class LogoutComponent implements OnInit, OnDestroy {
+  private _isDead$ = new Subject();
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  logout():void {
+    this.userService.logout().pipe(takeUntil(this._isDead$)).subscribe(
+      response => {
+        if (response) {
+          this.router.navigate([''])
+        }
+      }
+    )
+  }
+  ngOnDestroy(): void {
+    // @ts-ignore
+    this._isDead$.next();
+  }
 }
