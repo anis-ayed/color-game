1e915b1616b539c89be6eea9d5b37d794acc1b06
Anis <ayed.aniss@hotmail.com>
2021-12-06 11:19:39 +0100

    Q2-A- 15 min

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {LoadingComponent} from "./shared/loading/loading.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'loading', component: LoadingComponent},
+  {path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule)},
+  {path: 'game', loadChildren: () => import('./game/game.module').then(m => m.GameModule)},
+  {path: '**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
