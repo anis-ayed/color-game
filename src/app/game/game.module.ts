e0bb1ec36303f734bc672a4e5a2de8d1a584259b
Anis <ayed.aniss@hotmail.com>
2021-12-06 12:04:12 +0100

    Q3A-20min

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewComponent } from './new/new.component';
 import { CaseComponent } from './case/case.component';
+import {GameRoutingModule} from "./game-routing.module";
 
 
 
@@ -11,7 +12,8 @@ import { CaseComponent } from './case/case.component';
     CaseComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    GameRoutingModule
   ]
 })
 export class GameModule { }
--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Subject, takeUntil} from "rxjs";
+import {UserService} from "../user-service.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  public loginForm: FormGroup;
+  public errorMsg: string = '';
+  private _isDead$ = new Subject();
+  constructor(private userService: UserService, private router: Router) {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(6)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(5)])
+    })
+  }
 
   ngOnInit(): void {
   }
-
+  login(): void {
+    this.userService.login(this.loginForm.value).pipe(takeUntil(this._isDead$)).subscribe(
+      (user: any) => {
+        if(!!user) {
+          this.router.navigate([''])
+        }
+      }, error => {
+        this.errorMsg = error.message
+      })
+  }
+  ngOnDestroy(): void {
+    // @ts-ignore
+    this._isDead$.next();
+  }
 }
